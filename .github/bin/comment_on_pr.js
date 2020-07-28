76fc26eb45aab555ca8ea99af0da849fb63b3150
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2020-07-28 17:27:06 +0100

    Fix JSON.stringify lack of single quote escaping

--- .github/bin/comment_on_pr.js
+++ .github/bin/comment_on_pr.js
@@ -1,4 +1,4 @@
-const {execSync, curlOpts} = require('./util');
+const {execSync, stringify, curlOpts} = require('./util');
 const {env} = process;
 const pr_event = JSON.parse(env.PR_EVENT);
 
@@ -12,13 +12,13 @@ if (env.JOB_STATUS === 'success') {
 const nextSteps = `
 Next steps for @${pr_event.assignees[0].login}:
 - [view/edit the GitHub release description](${env.RELEASE_URL}) as appropriate
-- ${isMilestoneClosed ? '✔️ Milestone automatically closed' : 'ℹ️ Close the milestone'}
+- ${isMilestoneClosed ? '✔️ Milestone automatically closed' : 'Close the milestone'}
 `;
 
 const manualInstructions = `
 [Check the run](${pr_event.html_url}/checks)
 
-If you need to publish the dist to PyPI manually:
+You can still publish the dist to PyPI manually:
 
 (Make sure you have commit \`${env.MERGE_SHA}\` checked out)
 \`\`\`shell
@@ -47,15 +47,15 @@ const bodyText = () => {
     return [title, content, footer].join('');
 };
 
-const payload = JSON.stringify({
+const payload = {
     body: bodyText()
-});
+};
 
 execSync(`curl -X POST \
     ${pr_event.comments_url} \
     -H "authorization: Bearer $GITHUB_TOKEN" \
     -H "content-type: application/json" \
-    --data '${payload}' \
+    --data '${stringify(payload)}' \
     ${curlOpts}`
 );
 
@@ -63,15 +63,15 @@ execSync(`curl -X POST \
 
 function closeMilestone() {
     if (pr_event.milestone) {
-        const patch = JSON.stringify({
+        const payload = {
             state: 'closed'
-        });
+        };
         try {
             execSync(`curl -X PATCH \
                 https://api.github.com/repos/${env.REPOSITORY}/milestones/${pr_event.milestone.number} \
                 -H "authorization: Bearer $GITHUB_TOKEN" \
                 -H "content-type: application/json" \
-                --data '${patch}' \
+                --data '${stringify(payload)}' \
                 ${curlOpts}`
             );
             return true;
@@ -84,14 +84,14 @@ function closeMilestone() {
 }
 
 function updatePRTitle() {
-    const patch = JSON.stringify({
+    const payload = {
         title: `[${env.JOB_STATUS}] ${pr_event.title}`
-    });
+    };
     execSync(`curl -X PATCH \
         ${pr_event.url} \
         -H "authorization: Bearer $GITHUB_TOKEN" \
         -H "content-type: application/json" \
-        --data '${patch}' \
+        --data '${stringify(payload)}' \
         ${curlOpts}`
     );
 }
